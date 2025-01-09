d3046069a304774e19b87048e2c4f53b0c700290
Krzysztof Zalewski <krzysztof.zalewski@housecallpro.com>
2025-01-09 16:58:30 +0100

    Don't use Base in Env

--- lib/env.ml
+++ lib/env.ml
@@ -1,11 +1,8 @@
-open Base
-
 module Hashtbl_printable = struct
   type ('k, 's) t = ('k, 's) Hashtbl.t
 
   let pp pp_key pp_value ppf values =
-    Hashtbl.iteri values ~f:(fun ~key ~data ->
-        Stdlib.Format.fprintf ppf "@[<1>%a: %a@]@." pp_key key pp_value data)
+    values |> Hashtbl.iter (fun key data -> Stdlib.Format.fprintf ppf "@[<1>%a: %a@]@." pp_key key pp_value data)
 end
 
 type 'a env = {
@@ -14,15 +11,15 @@ type 'a env = {
 }
 [@@deriving show]
 
-let init () : 'a env = { store = Hashtbl.create (module String) ~size:10; outer = None }
+let init () : 'a env = { store = Hashtbl.create 10; outer = None }
 
 let rec get (e : 'a env) (key : string) : 'a option =
-  match Hashtbl.find e.store key with
+  match Hashtbl.find_opt e.store key with
   | Some v -> Some v
   | None -> ( match e.outer with Some outer -> get outer key | None -> None)
 
 let set e key val' =
-  Hashtbl.set e.store ~key ~data:val';
+  Hashtbl.replace e.store key val';
   e
 
-let wrap e = { store = Hashtbl.create (module String) ~size:10; outer = Some e }
+let wrap e = { store = Hashtbl.create 10; outer = Some e }
